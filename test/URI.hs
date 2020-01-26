229c03947d24e9073b3b29e06dfa89aad29960c3
Mario Blažević <blamario@protonmail.com>
2020-01-26 08:46:34 -0500

    Trimmed down the bug test

--- test/URI.hs
+++ test/URI.hs
@@ -1,5 +1,4 @@
-{-# LANGUAGE FlexibleInstances, LambdaCase, OverloadedStrings, ScopedTypeVariables, StandaloneDeriving,
-             TemplateHaskell, TupleSections, TypeApplications #-}
+{-# LANGUAGE OverloadedStrings, TupleSections #-}
 
 module URI where
 
@@ -7,7 +6,7 @@ import Data.Bits ((.|.), (.&.), shift)
 import Data.ByteString (ByteString)
 import qualified Data.ByteString as ByteString
 import qualified Data.ByteString.Char8 as ASCII
-import Data.Char (isAlpha, isAscii, isDigit, isHexDigit, chr, ord, toUpper)
+import Data.Char (isAlpha, isAlphaNum, isAscii, isDigit, isHexDigit, chr, ord, toUpper)
 import Data.Foldable (fold)
 import Data.Functor.Identity (Identity, runIdentity)
 import qualified Data.List as List
@@ -29,67 +28,17 @@ import OrphanInstances
 
 import Prelude hiding ((<$), (<*), (*>), take, takeWhile)
 
-data UriReference t f = UriReference{
-   scheme    :: f (Maybe t),
-   authority :: f (Maybe (Authority t Identity)),
-   path      :: f [t],
-   query     :: f (Maybe t),
-   fragment  :: f (Maybe t)
-   }
-
-data Authority t f = Authority{
-   user :: f (Maybe t),
-   host :: f (HostName t),
-   port :: f (Maybe Word16)
-   }
-
-data HostName t = IPv4address [Word8]
-                | IPv6address [Word16]
-                | IPvFuture Word t
-                | RegisteredName t
-                deriving (Eq, Read, Show)
-
-deriving instance Show t => Show (UriReference t Identity)
-deriving instance Show t => Show (Authority t Identity)
-
-main = print (completeResults $ feedEof $ feed "ftp://ftp.is.co.za/rfc/rfc1808.txt" $ parse uriReference)
-
-uriReference :: Format (Parser ByteString) Maybe ByteString (UriReference ByteString Identity)
-uriReference = record UriReference{
-   scheme = optional (uriScheme <* literal ":"),
-   authority = optional (literal "//" *> uriAuthority),
-   path = encodedCharSequence pathChar `sepBy` literal "/",
-   query = optional (literal "?" *> encodedCharSequence queryChar),
-   fragment = optional (literal "#" *> encodedCharSequence fragmentChar)
-   }
-
-uriAuthority :: Format (Parser ByteString) Maybe ByteString (Authority ByteString Identity)
-uriAuthority = record Authority{
-        user = optional (takeCharsWhile userChar <* literal "@"),
-        host = hostName,
-        port = optional (mapValue fromIntegral fromIntegral $
-                         satisfy (< 65536) $ literal ":" *> mapDec (takeCharsWhile1 digit))}  -- port = *DIGIT in spec?
+main = print (completeResults $ feedEof $ feed "ftp://ftp.is.co.za/rfc/rfc1808.txt" $ parse uriScheme)
 
 uriScheme :: Format (Parser ByteString) Maybe ByteString ByteString
 uriScheme = mapValue (uncurry (<>)) (Factorial.splitAt 1) $
-            pair (satisfy (any alpha . Textual.characterPrefix) $ take 1) (takeCharsWhile schemeChar)
-                      
-hostName :: Format (Parser ByteString) Maybe ByteString (HostName ByteString)
-hostName = mapMaybeValue (Just . IPv4address) (\case (IPv4address a)-> Just a; _ -> Nothing) ipV4address
-           <|> literal "["
-               *> (mapMaybeValue (Just . IPv6address) (\case (IPv6address a)-> Just a; _ -> Nothing) ipV6address
-                    <|> mapMaybeValue (Just . uncurry IPvFuture) (\case (IPvFuture v a)-> Just (v, a); _ -> Nothing)
-                                      (pair (literal "v" *> mapHex (takeCharsWhile1 hexDigit))
-                                            (literal "." *> takeCharsWhile1 ipFutureChar)))
-               <* literal "]"
-           <|> mapMaybeValue (Just . RegisteredName) (\case (RegisteredName a)-> Just a; _ -> Nothing)
-                             (encodedCharSequence hostChar)
+            pair (satisfy (any isAlpha . Textual.characterPrefix) $ take 1) (takeCharsWhile isAlphaNum)
 
 ipV4address :: Format (Parser ByteString) Maybe ByteString [Word8]
 ipV4address = satisfy ((== 4) . length) (decOctet `sepBy` literal ".")
    where decOctet = mapValue fromIntegral fromIntegral $
                     satisfy (< 256) $ mapDec $
-                    takeCharsWhile1 digit
+                    takeCharsWhile1 isDigit
 
 ipV6address :: Format (Parser ByteString) Maybe ByteString [Word16]
 ipV6address = satisfy ((== 8) . length) $
@@ -114,47 +63,10 @@ ipV6addressShort = satisfy zeroOrOneNothings $
          v6tov4 (Nothing:words) = 0 : 0 : v6tov4 words
          zeroOrOneNothings words = elisionCount == 0 && length words == 8 || elisionCount == 1 && length words < 7
             where elisionCount = length (filter isNothing words)
-         h16 = mapHex $ satisfy ((<5) . Factorial.length) $ takeCharsWhile1 hexDigit
+         h16 = mapHex $ satisfy ((<5) . Factorial.length) $ takeCharsWhile1 isHexDigit
 
 mapHex :: (Integral n, Show n) =>
           Format (Parser ByteString) Maybe ByteString ByteString -> Format (Parser ByteString) Maybe ByteString n
 mapDec :: Format (Parser ByteString) Maybe ByteString ByteString -> Format (Parser ByteString) Maybe ByteString Natural
 mapDec = mapValue (read . Textual.toString (error . show)) (fromString . show)
 mapHex = mapValue (fst . head . readHex . Textual.toString (error . show)) (fromString . flip showHex "")
-
-alpha, digit, hexDigit, schemeChar, hostChar, userChar, ipFutureChar,
-   pathChar, queryChar, fragmentChar, unreserved, subDelim :: Char -> Bool
-
-alpha c      = isAlpha c && isAscii c
-digit c      = isAlpha c && isDigit c
-hexDigit c   = isHexDigit c && isAscii c
-schemeChar c = (isAlpha c || isDigit c) && isAscii c || elem @[] c "+-."
-ipFutureChar = userChar
-hostChar c   = unreserved c || subDelim c
-userChar c   = unreserved c || subDelim c || c == ':'
-pathChar c   = unreserved c || subDelim c || c == ':' || c == '@'
-queryChar c  = pathChar c || c == '/' || c == '?'
-fragmentChar = queryChar
-subDelim c   = elem @[] c "!$&'()*+,;="
-unreserved c = isAscii c && (isAlpha c || isDigit c || elem @[] c "-._~")
-
-encodedCharSequence :: (Char -> Bool) -> Format (Parser ByteString) Maybe ByteString ByteString
-encodedCharSequence predicate = mapValue concatSequence splitSequence $
-                                many (takeCharsWhile1 predicate <+> percentEncoded)
-   where concatSequence :: [Either ByteString Char] -> ByteString
-         splitSequence :: ByteString -> [Either ByteString Char]
-         percentEncoded :: Format (Parser ByteString) Maybe ByteString Char
-         concatSequence = mconcat . map (either id Textual.singleton)
-         splitSequence s = case Textual.splitCharacterPrefix s
-            of Just (c, t)
-                  | predicate c, (prefix, rest) <- Textual.span_ False predicate s -> Left prefix : splitSequence rest
-                  | otherwise -> Right c : splitSequence t
-               _ -> []
-         percentEncoded = mapValue hexChar (padLeft . map toUpper . (`showHex` "") . ord) $
-                          literal "%" *> count 2 (satisfy hexDigit char)
-         hexChar = chr . fst . head . readHex
-         padLeft [c] = ['0', c]
-         padLeft cs = cs
-
-$(Rank2.TH.deriveAll ''UriReference)
-$(Rank2.TH.deriveAll ''Authority)
